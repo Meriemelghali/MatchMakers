2d18b42c24119b57ede41c4a88ae93d8aa47c59e
Ikrambenhammouda <ikram.benhammouda@esprit.tn>
2026-03-15 23:20:35 +0100

    userService FrontEnd

---
+++ Frontend/src/app/Auth/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService, LoginRequest } from 'src/app/services/auth.service';
+import { NgForm } from '@angular/forms'; 
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loading = false;
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) { }
+
+  onLogin(form: NgForm) {
+    if (form.invalid) return;
+
+    this.loading = true;
+    this.errorMessage = '';
+
+    const request: LoginRequest = {
+      email: form.value.email,
+      password: form.value.password
+    };
+
+    this.authService.login(request).subscribe({
+      next: (res) => {
+        this.authService.saveTokens(res);
+        this.router.navigate(['/matches']); // après login -> page matches
+      },
+      error: (err) => {
+        this.loading = false;
+        this.errorMessage = err.error?.message || 'Erreur de connexion';
+      },
+      complete: () => this.loading = false
+    });
+  
+  }
+
+
+}
+
+
