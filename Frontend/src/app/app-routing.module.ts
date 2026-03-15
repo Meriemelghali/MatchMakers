58163603704f47344be89d1c2bdaf99100f899af
Ikrambenhammouda <ikram.benhammouda@esprit.tn>
2026-03-15 23:20:35 +0100

    userService FrontEnd

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicLayoutComponent } from './public-layout/public-layout.component';
+import { LoginComponent } from './Auth/login/login.component';
+import { AuthLayoutComponent } from './auth-layout/auth-layout.component';
+import { authGuard } from './guards/auth.guard';  
 
 const routes: Routes = [
+  //par défaut 
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
   {
-    path: 'matches',
-    loadChildren: () => import('./matches/matches.module').then(m => m.MatchesModule)
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      { path: 'login', component: LoginComponent },
+      // autres pages publiques
+    ]
   },
   {
-    path: 'terrains',
-    loadChildren: () => import('./terrains/terrains.module').then(m => m.TerrainsModule)
+    path: '',
+    component: AuthLayoutComponent,
+    canActivate: [authGuard], // protège toutes les routes ici
+    children: [
+      {
+        path: 'matches',
+        loadChildren: () =>
+          import('./matches/matches.module').then(m => m.MatchesModule)
+      },
+      {
+        path: 'terrains',
+        loadChildren: () =>
+          import('./terrains/terrains.module').then(m => m.TerrainsModule)
+      },
+      // autres routes privées ici
+    ]
   },
-  { path: '', redirectTo: 'matches', pathMatch: 'full' }
 ];
 
 @NgModule({
