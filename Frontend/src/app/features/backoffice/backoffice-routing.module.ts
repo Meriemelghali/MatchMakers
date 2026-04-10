3eda540f91656b56f5c5ef61ae7eaab4d82166d3
Ikrambenhammouda <ikram.benhammouda@esprit.tn>
2026-04-10 21:32:26 +0100

    roles

--- Frontend/src/app/features/backoffice/backoffice-routing.module.ts
+++ Frontend/src/app/features/backoffice/backoffice-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './user-list/user-list.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { RoleListComponent } from './roles/role-list/role-list.component';
+import { RoleFormComponent } from './roles/role-form/role-form.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'users', component: UserListComponent },
+  { path: 'roles', component: RoleListComponent },
+  { path: 'roles/add', component: RoleFormComponent },
+  { path: 'roles/edit/:id', component: RoleFormComponent },
 ];
 
 @NgModule({
